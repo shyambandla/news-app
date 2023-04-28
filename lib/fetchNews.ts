dfbc6df1851a4683818d69a7aa2ce08bcb628675
shyambandla <shyambandla143@gmail.com>
2023-04-29 04:56:04 +0530

    update

--- lib/fetchNews.ts
+++ lib/fetchNews.ts
@@ -1,5 +1,6 @@
 import { gql } from "graphql-request";
 import sortNewsByImage from "./sortNewsByImage";
+import axios from 'axios';
 
 const fetchNews = async (
   category?: Category | string,
@@ -8,72 +9,75 @@ const fetchNews = async (
 ) => {
   // GraphQL query
 
-  const query = gql`
-    query MyQuery(
-      $access_key: String!
-      $categories: String!
-      $keywords: String
-    ) {
-      myQuery(
-        access_key: $access_key
-        categories: $categories
-        countries: "us"
-        sort: "published_desc"
-        keywords: $keywords
-      ) {
-        data {
-          author
-          category
-          image
-          description
-          country
-          language
-          published_at
-          source
-          title
-          url
-        }
-        pagination {
-          count
-          limit
-          offset
-          total
-        }
-      }
-    }
-  `;
+  // const query = gql`
+  //   query MyQuery(
+  //     $access_key: String!
+  //     $categories: String!
+  //     $keywords: String
+  //   ) {
+  //     myQuery(
+  //       access_key: $access_key
+  //       categories: $categories
+  //       countries: "us"
+  //       sort: "published_desc"
+  //       keywords: $keywords
+  //     ) {
+  //       data {
+  //         author
+  //         category
+  //         image
+  //         description
+  //         country
+  //         language
+  //         published_at
+  //         source
+  //         title
+  //         url
+  //       }
+  //       pagination {
+  //         count
+  //         limit
+  //         offset
+  //         total
+  //       }
+  //     }
+  //   }
+  // `;
 
   // Fetch function with Next.js 13 caching...
 
-  const res = await fetch(
-    "https://muritiba.stepzen.net/api/idolized-gnat/__graphql",
-    {
-      method: "POST",
-      cache: isDynamic ? "no-cache" : "default",
-      next: isDynamic ? { revalidate: 0 } : { revalidate: 20 },
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Apikey ${process.env.STEPZEN_API_KEY}`,
-      },
-      body: JSON.stringify({
-        query,
-        variables: {
-          access_key: process.env.MEDIASTACK_API_KEY,
-          categories: category,
-          keywords: keywords,
-        },
-      }),
-    }
-  );
+  // const res = await fetch(
+  //   "https://linkshortner.me/api/news/top",
+  //   {
+  //     method: "POST",
+  //     cache: isDynamic ? "no-cache" : "default",
+  //     next: isDynamic ? { revalidate: 0 } : { revalidate: 20 },
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //       Authorization: `Apikey ${process.env.STEPZEN_API_KEY}`,
+  //     },
+  //     body: JSON.stringify({
+  //       query,
+  //       variables: {
+  //         access_key: process.env.MEDIASTACK_API_KEY,
+  //         categories: category,
+  //         keywords: keywords,
+  //       },
+  //     }),
+  //   }
+  // );
 
-  const newsResponse = await res.json();
+  // const newsResponse = await res.json();
+
+
   // Sort function by images vs not images present
+  const news=await axios.get("http://linkshortner.me/api/news/top");
 
-  const news = sortNewsByImage(newsResponse.data.myQuery);
+  // const news = sortNewsByImage(newsResponse.data);
 
   // return news;
 
-  return news;
+  return news.data;
 };
 
 export default fetchNews;
